385591782c34ffbc87c454070691c7646678955e
Angela Mumba <malaika.mumba@gmail.com>
2019-05-21 20:06:10 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,25 @@
+class Song 
+  
+  attr_accessor :name, :artist, :genre 
+  @@count = 0
+  @@genres = []
+  @@artists = []
+
+  def initialize(name,artist,genre)
+    @name = name
+    @artist = artist
+    @genre = genre 
+    @@count +=1
+    @@artists << self.artist
+    @@genres << self.genre
+  end 
+  
+  def count 
+    Song.count
+  end 
+  
+#   def artist_count 
+    
+    
+#   end 
+end 
